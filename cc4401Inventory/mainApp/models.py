149afddb9ed87bad0efecfabcf6bc4cec89ed1fd
Damian Arquez <demian_ms@live.cl>
2018-05-03 17:39:27 -0300

    Agregar modelos base para Item y Action

--- cc4401Inventory/mainApp/models.py
+++ cc4401Inventory/mainApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField('Nombre', max_length=40)
+    description = models.TextField('Descripción', blank=True)
+    image = models.ImageField('Imagen del articulo', upload_to='images/items', blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Action(models.Model):
+    STATES = (
+        ('A', 'Aceptado'),
+        ('R', 'Rechazado'),
+        ('P', 'Pendiente')
+    )
+    starting_date_time = models.DateTimeField()
+    ending_date_time = models.DateTimeField()
+    state = models.CharField('Estado', choices=STATES, max_length=1, default='P')
