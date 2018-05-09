40a5b29d879857adf7b6e6d79547d3114b43c717
nevz <sreisenegger@gmail.com>
2018-05-09 17:04:49 -0300

    agregar html template para artiuclos

--- cc4401Inventory/articlesApp/views.py
+++ cc4401Inventory/articlesApp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def index(request):
+    #items = Item.objects.all()
+    #context = {'items': items}
+    return render(request, 'articlesApp.html')
