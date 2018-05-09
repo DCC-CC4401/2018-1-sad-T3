7cbd1f87bbdb12a88c976fb24fc172c45400c429
Damian Arquez <demian_ms@live.cl>
2018-05-08 23:03:39 -0300

    Crear vista del panel de administracion de usarios

--- cc4401Inventory/cc4401Inventory/settings.py
+++ cc4401Inventory/cc4401Inventory/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'cc4401Inventory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'adminApp/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
