77813da929ac2e1d4d2949f6cd2b373b516be863
Darío Cáceres <dignaciocaceres@gmail.com>
2018-06-28 17:15:35 -0400

    Formulario busqueda
    se agrega view para testear envio de datos del formulario

--- cc4401Inventory/usersApp/urls.py
+++ cc4401Inventory/usersApp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('landingtest', views.landing_test, name='testlndng'),
+    path('landing', views.landing, name='landing'),
+    path('buscar/', views.buscar, name='buscar'),
 ]
+
--- cc4401Inventory/usersApp/views.py
+++ cc4401Inventory/usersApp/views.py
@@ -5,3 +5,21 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'user_profile.html', context)
+
+def landing(request):
+    context = {}
+    return render(request, 'landing.html', context)
+
+def landing_test(request):
+    context = {}
+    return render(request, 'usersApp/landing_user.html', context)
+
+
+def buscar(request):
+
+    if request.method == "POST":
+        return landing_test(request)
+    context = {}
+    return landing(request)
+
+
