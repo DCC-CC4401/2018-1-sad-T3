20cd215e30886c23dba4cd7892c919f0df17a129
Darío Cáceres <dignaciocaceres@gmail.com>
2018-07-01 23:09:57 -0400

    Initialized static directory
    + Initialized static dir.
    + Updated README.md

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 Proyecto Git para la _Tarea 3_ del grupo 5 **__SAD Development__**.
 
+Instrucciones para ejecutar el proyecto:
+
+1. Crear un nuevo *entorno virtual*
+
+2. Instalar las dependencias usando:
+    >pip install -r requirements.txt
+
+3. Ejecutar usando:
+    >python manage.py runserver
+     
+*Nota: Para servir los archivos estáticos, debe mantenerse activo el modo de desarrollo, es decir, no modificar la variable debug en **settings.py**.*
--- cc4401Inventory/cc4401Inventory/urls.py
+++ cc4401Inventory/cc4401Inventory/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('staff/', include('adminApp.urls')),
     path('articles/', include('articlesApp.urls')),
     path('', include('usersApp.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
