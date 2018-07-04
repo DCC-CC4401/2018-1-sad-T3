9c86a4de3fbdb983eb531155e33a79c5cda27cce
Damian Arquez <demian_ms@live.cl>
2018-07-04 19:14:00 -0400

    Refactor articlesApp

--- cc4401Inventory/articlesApp/urls.py
+++ cc4401Inventory/articlesApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'list'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('/<int:article_id>', views.article_data, name='article_data'),
+    path('/<int:article_id>/edit', views.article_data_admin, name='article_data_admin'),
 ]
--- cc4401Inventory/articlesApp/views.py
+++ cc4401Inventory/articlesApp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render, redirect
+from articlesApp.models import Article
 
 
-# Create your views here.
-def index(request):
-    #items = Item.objects.all()
-    #context = {'items': items}
-    return render(request, 'login.html')
+def article_data(request, article_id):
+    try:
+        article = Article.objects.get(id=article_id)
+        context = {
+            'article': article
+        }
+        return render(request, 'article_data.html', context)
+    except:
+        return redirect('/')
+
+
+def article_data_admin(request, article_id):
+    if not request.user.is_staff:
+        return redirect('/')
+    else:
+        try:
+            article = Article.objects.get(id=article_id)
+            context = {
+                'article': article
+            }
+            return render(request, 'article_data_admin.html', context)
+        except:
+            return redirect('/')
