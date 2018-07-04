95b05b17cf06c4f31b86aa367bbe1c8f8c139068
Darío Cáceres <dignaciocaceres@gmail.com>
2018-07-04 16:29:53 -0400

    Working Space Reservations
    + Fixed timezone

--- cc4401Inventory/usersApp/views.py
+++ cc4401Inventory/usersApp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
-#hay que importar los modelos
+from django.utils.timezone import localtime
+import datetime
 from articlesApp.models import Article
+from reservationsApp.models import Reservation
 
 # Create your views here.
 
@@ -19,17 +20,51 @@ def index(request):
     context = {}
     return render(request, 'user_profile.html', context)
 
+
+
+
+
 def landing_articulos(request):
     context = {}
     return render(request, 'articulos.html', context)
 
-def landing_espacios(request):
-    context = {}
+def landing_espacios(request, date = None):
+
+    if date:
+        current_date = date
+        current_week = datetime.datetime.strptime(current_date,"%Y-%m-%d").date().isocalendar()[1]
+    else:
+        try:
+            current_week = datetime.datetime.strptime(request.GET["date"], "%Y-%m-%d").date().isocalendar()[1]
+            current_date = request.GET["date"]
+        except:
+            current_week = datetime.date.today().isocalendar()[1]
+            current_date = datetime.date.today().strftime("%Y-%m-%d")
+
+    reservations = Reservation.objects.filter(starting_date_time__week = current_week, state__in = ['P','A'])
+    colores = {'A': 'rgba(0,153,0,0.7)',
+               'P': 'rgba(51,51,204,0.7)'}
+
+    res_list = []
+    for i in range(5):
+        res_list.append(list())
+    for r in reservations:
+        reserv = []
+        reserv.append(r.space.name)
+        reserv.append(localtime(r.starting_date_time).strftime("%H:%M"))
+        reserv.append(localtime(r.ending_date_time).strftime("%H:%M"))
+        reserv.append(colores[r.state])
+        res_list[r.starting_date_time.isocalendar()[2]-1].append(reserv)
+
+    move_controls = list()
+    move_controls.append((datetime.datetime.strptime(current_date,"%Y-%m-%d")+datetime.timedelta(weeks=-4)).strftime("%Y-%m-%d"))
+    move_controls.append((datetime.datetime.strptime(current_date,"%Y-%m-%d")+datetime.timedelta(weeks=-1)).strftime("%Y-%m-%d"))
+    move_controls.append((datetime.datetime.strptime(current_date,"%Y-%m-%d")+datetime.timedelta(weeks=1)).strftime("%Y-%m-%d"))
+    move_controls.append((datetime.datetime.strptime(current_date,"%Y-%m-%d")+datetime.timedelta(weeks=4)).strftime("%Y-%m-%d"))
+
+    context = {'reservations' : res_list, 'current_date' : current_date, 'controls' : move_controls}
     return render(request, 'espacios.html', context)
 
-def landing_test(request):
-    context = {}
-    return render(request, 'usersApp/landing_user.html', context)
 
 def landing_search(request, productos):
     if not productos:
@@ -49,10 +84,10 @@ def buscar(request):
 
     if request.method == "GET":
         query = request.GET['query']
-        a_type = "filo"
+        #a_type = "comportamiento_no_definido"
         a_state = "A" if (request.GET['estado'] == "A") else request.GET['estado']
 
-        # TODO: REALIZAR BUSQUEDA DE LOS PRODUCTOS
+
 
         if not (a_state == "A"):
             articles = Article.objects.filter(state=a_state,name__icontains=query.lower())
@@ -63,6 +98,4 @@ def buscar(request):
         return landing_search(request, productos)
 
 
-def get_spaces(request):
 
-        return landing_espacios(request)
