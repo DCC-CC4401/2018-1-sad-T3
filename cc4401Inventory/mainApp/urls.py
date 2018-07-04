cba8351e137e73c70586e3765b27445ba1060923
Damian Arquez <demian_ms@live.cl>
2018-07-04 18:57:09 -0400

    Refactor: Move articles and spaces views to MainApp

--- cc4401Inventory/mainApp/urls.py
+++ cc4401Inventory/mainApp/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from . import views
+
+url_patterns = [
+    path('/articles', views.landing_articles, name='landing_articles'),
+    path('/spaces', views.landing_spaces, name='landing_spaces'),
+    path('/search', views.search, name='search')
+]
