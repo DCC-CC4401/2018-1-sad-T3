e0faf96f32497b029341c18a1e23472116e2568d
nevz <sreisenegger@gmail.com>
2018-07-07 17:44:36 -0400

    Agregar metodo para logear usuarios

--- cc4401Inventory/usersApp/urls.py
+++ cc4401Inventory/usersApp/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('<int:user_id>/', views.user_data, name='user_data'),
     path('signup/', views.sign_up, name='sign_up'),
-    path('login/', views.login, name='login'),
+    path('login_page/', views.login_page, name='login_page'),
+    path('login/', views.login_user, name='login'),
 ]
 
--- cc4401Inventory/usersApp/views.py
+++ cc4401Inventory/usersApp/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from mainApp.models import User
+from django.http import HttpResponse
 
 
-def login(request):
+def login_page(request):
     if request.method == 'GET':
         return render(request, 'usersApp/login.html')
     if request.method == 'POST':
         pass
+
+
+def login_user(request):
+
+    username = request.POST['email']
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        username = user.get_full_name()
+        email = user.get_email()
+
+        context = {'username': username,
+                   'email' : email}
+        #aca hay que redirigir a la pagina de inicio del usuario
+        return render(request, 'usersApp/success.html', context=context)
+
+
+    else:
+        return HttpResponse("Error, invalid user")
+        # Return an 'invalid login' error message.
 
 
 def sign_up(request):
