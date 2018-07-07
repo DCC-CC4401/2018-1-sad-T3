d5f00705028f3d20bdfa0009228661299cf46b5c
nevz <sreisenegger@gmail.com>
2018-07-07 19:15:08 -0400

    agregar error de login y cambios menores a urls

--- cc4401Inventory/usersApp/urls.py
+++ cc4401Inventory/usersApp/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('<int:user_id>/', views.user_data, name='user_data'),
     path('signup/', views.signup, name='signup'),
-    path('login_page/', views.login_page, name='login_page'),
-    path('login/', views.login_submit, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('login/submit/', views.login_submit, name='login_submit'),
     path('signup/submit/', views.signup_submit, name='signup_submit')
 ]
 
--- cc4401Inventory/usersApp/views.py
+++ cc4401Inventory/usersApp/views.py
@@ -4,9 +4,10 @@ from mainApp.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 
 
-def login_page(request):
+def login_view(request):
+    context = {}
     if request.method == 'GET':
-        return render(request, 'usersApp/login.html')
+        return render(request, 'usersApp/login.html', context=context)
     if request.method == 'POST':
         pass
 
@@ -15,24 +16,20 @@ def login_submit(request):
 
     username = request.POST['email']
     password = request.POST['password']
-
     user = authenticate(request, username=username, password=password)
+    context = {'error_message': ''}
 
     if user is not None:
         login(request, user)
-        username = user.get_full_name()
-        email = user.get_email()
 
-        context = {'username': username,
-                   'email' : email}
+        context = {'user':user}
         #aca hay que redirigir a la pagina de inicio del usuario
         return render(request, 'usersApp/success.html', context=context)
 
-
     else:
-        return HttpResponse("Error, invalid user")
-        # Return an 'invalid login' error message.
-
+        error_message = 'La contraseña ingresada no es correcta o el usuario no existe'
+        context['error_message'] = error_message
+        return render(request, 'usersApp/login.html', context=context)
 
 #se llama cuando se quiere acceder a la pagina de creacion de cuentas
 def signup(request):
@@ -43,6 +40,9 @@ def signup(request):
 
 #se llama cuando se manda el formulario de creacion de cuentas
 def signup_submit(request):
+
+    context = {'error_message': '', }
+
     if(request.method == 'POST'):
         email = request.POST['email']
         first_name = request.POST['first_name']
