3a3c941a227dc7eaabd455438961aa567bad1063
Darío Cáceres <dignaciocaceres@gmail.com>
2018-07-08 01:05:26 -0400

    Added user article view backend
    Also fixed frontend

--- cc4401Inventory/articlesApp/views.py
+++ cc4401Inventory/articlesApp/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, redirect
 from articlesApp.models import Article
-
+from loansApp.models import Loan
+from datetime import datetime
+import pytz
 
 def article_data(request, article_id):
     try:
         article = Article.objects.get(id=article_id)
+        last_loans = Loan.objects.filter(article=article,
+                                         ending_date_time__lt=datetime.now(tz=pytz.utc)
+                                         ).order_by('-ending_date_time')[:10]
+        loan_list = list()
+        for loan in last_loans:
+            starting_day = loan.starting_date_time.strftime("%d-%m-%Y")
+            ending_day = loan.ending_date_time.strftime("%d-%m-%Y")
+            starting_hour = loan.starting_date_time.strftime("%H:%M")
+            ending_hour = loan.ending_date_time.strftime("%H:%M")
+
+            if starting_day == ending_day:
+                loan_list.append(starting_day+" "+starting_hour+" a "+ending_hour)
+            else:
+                loan_list.append(starting_day + ", " + starting_hour + " a " +ending_day + ", " +ending_hour)
+
+
         context = {
-            'article': article
+            'article': article,
+            'last_loans': loan_list
         }
         return render(request, 'article_data.html', context)
     except:
