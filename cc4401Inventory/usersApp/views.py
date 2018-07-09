0b948ef3e2985cb9b3f177e9a5b909cedb2ae53f
Araceli-Queirolo <araceli.queirolo@gmail.com>
2018-07-09 15:02:01 -0400

    Casillas solo en los pendientes

--- cc4401Inventory/usersApp/views.py
+++ cc4401Inventory/usersApp/views.py
@@ -72,12 +72,12 @@ def logout_view(request):
 def user_data(request, user_id):
     try:
         user = User.objects.get(id=user_id)
-        reservation = Reservation.objects.filter(user = user_id).order_by('-starting_date_time')[:10]
-        loan = Loan.objects.filter(user = user_id).order_by('-starting_date_time')[:10]
+        reservations = Reservation.objects.filter(user = user_id).order_by('-starting_date_time')[:10]
+        loans = Loan.objects.filter(user = user_id).order_by('-starting_date_time')[:10]
         context = {
             'user': user,
-            'reservation': reservation,
-            'loan': loan
+            'reservations': reservations,
+            'loans': loans
         }
         return render(request, 'user_profile.html', context)
     except:
