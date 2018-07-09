caf90efe5df9a8fee20d7602e6ed673490e76921
Darío Cáceres <dignaciocaceres@gmail.com>
2018-07-09 19:57:39 -0400

    Added grid to admin, reservation management and loan filtering

--- cc4401Inventory/adminApp/views.py
+++ cc4401Inventory/adminApp/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from reservationsApp.models import Reservation
 from loansApp.models import Loan
 from articlesApp.models import Article
 from spacesApp.models import Space
 from mainApp.models import User
-
+from datetime import datetime, timedelta, date
+import pytz
+from django.utils.timezone import localtime
 
 @login_required
 def user_panel(request):
@@ -27,10 +29,88 @@ def items_panel(request):
 
 @login_required
 def actions_panel(request):
-    reservations = Reservation.objects.all()
-    loans = Loan.objects.all()
+
+    try:
+        current_week = datetime.strptime(request.GET["date"], "%Y-%m-%d").date().isocalendar()[1]
+        current_date = request.GET["date"]
+    except:
+        current_date = date.today().strftime("%Y-%m-%d")
+        current_week = date.today().isocalendar()[1]
+
+    colores = {'A': 'rgba(0,153,0,0.7)',
+               'P': 'rgba(51,51,204,0.7)',
+                'R': 'rgba(153, 0, 0,0.7)'}
+
+    reservations = Reservation.objects.filter(state='P').order_by('starting_date_time')
+    current_week_reservations = reservations = Reservation.objects.filter(starting_date_time__week = current_week)
+    actual_date = datetime.now(tz=pytz.utc)
+    try:
+        if request.method == "GET":
+            if request.GET["filter"]=='vigentes':
+                loans = Loan.objects.filter(ending_date_time__gt=actual_date).order_by('starting_date_time')
+            elif request.GET["filter"]=='caducados':
+                loans = Loan.objects.filter(ending_date_time__lt=actual_date, article__state='P').order_by('starting_date_time')
+            elif request.GET["filter"]=='perdidos':
+                loans = Loan.objects.filter(ending_date_time__lt=actual_date, article__state='L').order_by('starting_date_time')
+            else:
+                loans = Loan.objects.all().order_by('starting_date_time')
+    except:
+        loans = Loan.objects.all().order_by('starting_date_time')
+
+    res_list = []
+    for i in range(5):
+        res_list.append(list())
+    for r in current_week_reservations:
+        reserv = list()
+        reserv.append(r.space.name)
+        reserv.append(localtime(r.starting_date_time).strftime("%H:%M"))
+        reserv.append(localtime(r.ending_date_time).strftime("%H:%M"))
+        reserv.append(colores[r.state])
+        res_list[r.starting_date_time.isocalendar()[2] - 1].append(reserv)
+
+    move_controls = list()
+    move_controls.append(
+        (datetime.strptime(current_date, "%Y-%m-%d") + timedelta(weeks=-4)).strftime("%Y-%m-%d"))
+    move_controls.append(
+        (datetime.strptime(current_date, "%Y-%m-%d") + timedelta(weeks=-1)).strftime("%Y-%m-%d"))
+    move_controls.append(
+        (datetime.strptime(current_date, "%Y-%m-%d") + timedelta(weeks=1)).strftime("%Y-%m-%d"))
+    move_controls.append(
+        (datetime.strptime(current_date, "%Y-%m-%d") + timedelta(weeks=4)).strftime("%Y-%m-%d"))
+
+    delta = (datetime.strptime(current_date, "%Y-%m-%d").isocalendar()[2]) - 1
+    monday = (
+        (datetime.strptime(current_date, "%Y-%m-%d") - timedelta(days=delta)).strftime("%d/%m/%Y"))
+
+
     context = {
-        'reservations': reservations,
-        'loans': loans
+        'reservations_query': reservations,
+        'loans': loans,
+        'reservations': res_list,
+        'current_date': current_date,
+        'controls': move_controls,
+        'actual_monday': monday
     }
     return render(request, 'actions_panel.html', context)
+
+def modify_reservations(request):
+
+    if request.method == "POST":
+
+        accept = True if (request.POST["accept"] == "1") else False
+        reservations = Reservation.objects.filter(id__in=request.POST["selected"])
+        if accept:
+            for reservation in reservations:
+                reservation.state = 'A'
+                reservation.save()
+        else:
+            for reservation in reservations:
+                reservation.state = 'R'
+                reservation.save()
+
+    return redirect('/admin/actions-panel')
+
+
+
+
+
